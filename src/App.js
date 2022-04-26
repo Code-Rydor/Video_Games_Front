7413767d3012121813d93228b883a13ff80602c0
Garrett <ga.landin89@gmail.com>
2022-04-26 16:56:51 -0500

    Commented out some problem code + Instantiated DisplayConsoleSales component on App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 import SearchBar from './components/SearchBar/SearchBar';
+import DisplayConsoleSales from './components/DisplayConsoleSales/DisplayConsoleSales';
 
 
 function App() {
 
   const [games, setGames] = useState([])
   const [input, setInput] = useState('')
+  let requestReload = true
 
-  async function getAllGames() {
-    let response = await axios.get("https://localhost:7260/api/games");
-    setGames(response.data);
-  }
+  // async function getAllGames() {
+  //   let response = await axios.get("https://localhost:7260/api/games");
+  //   setGames(response.data);
+  // }
   
   const searchInput = (event) => {
     setInput(event.target.value)
   }
 
+  // useEffect(() => {
+  //   if (requestReload) {
+  //     getAllGames();
+  //     requestReload = !requestReload
+  //   }
+  // }, [requestReload])
 
 
   return (
     <div>
       <SearchBar searchInput={searchInput} input={input} />
+      <DisplayConsoleSales allGameData={games} input={input} />
     </div>
   );
 }
--- src/components/DisplayConsoleSales/DisplayConsoleSales.jsx
+++ src/components/DisplayConsoleSales/DisplayConsoleSales.jsx
@@ -1,21 +1,21 @@
-import React from "react";
 import { Chart } from "react-google-charts";
 
-const DisplayConsoleSales = (props) => {
+const DisplayConsoleSales = ({allGameData, input}) => {
 
-// const data = [
-//   ["Task", "Hours per Day"],
-//   ["Work", 11],
-//   ["Eat", 2],
-//   ["Commute", 2],
-//   ["Watch TV", 2],
-//   ["Sleep", 7],
-// ];
 
-// const options = {
-//   title: "My Daily Activities",
-//   is3D: true; 
-// };
+const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+const options = {
+  title: "My Daily Activities",
+  is3D: true 
+};
 
 
 return (
