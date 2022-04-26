7d2204eccf8ee93b4baaa3ef0489278893ee125a
Garrett <ga.landin89@gmail.com>
2022-04-26 16:32:48 -0500

    Created getAllGames axios request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useState } from 'react';
 
 
 function App() {
+
+  const [games, setGames] = useState([])
+
+  async function getAllGames() {
+    let response = await axios.get("https://localhost:7260/api/games");
+    setGames(response.data);
+}
+
+
+
   return (
     <div>
       <h3>Hello World</h3>
