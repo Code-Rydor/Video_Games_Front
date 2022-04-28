ba9576cbd4e70dcb89ba71d0473b3dd2021880d5
Garrett <ga.landin89@gmail.com>
2022-04-28 14:32:14 -0500

    Finished logic to calculate global sales by console since 2013

--- src/components/DisplayConsoleSales/DisplayConsoleSales.jsx
+++ src/components/DisplayConsoleSales/DisplayConsoleSales.jsx
@@ -6,52 +6,52 @@ const DisplayConsoleSales = ({ games }) => {
 
   function generateDataForChart() {
 
-    console.log(games);
+    // console.log(games);
 
     let filteredGames = games.filter(game => game.year >= 2013)
 
-    console.log('Filtered Games', filteredGames)
+    // console.log('Filtered Games', filteredGames)
 
     let platforms = filteredGames.map(game => {
       return game.platform
     });
 
-    console.log('Platforms', platforms)
+    // console.log('Platforms', platforms)
 
     let distinctPlatforms = [...new Set(platforms)]
 
-    console.log('Distinct Platform', distinctPlatforms)
+    // console.log('Distinct Platform', distinctPlatforms)
 
     let platformArrays = distinctPlatforms.map(platform => {
 
       let allGamesForPlatform = filteredGames.filter(game => game.platform === platform);
       
-      console.log("All games for platform:", allGamesForPlatform)
+      // console.log("All games for platform:", allGamesForPlatform)
 
       let globalSalesForPlatform = allGamesForPlatform.map(game => {
         return game.globalSales
       });
 
-      console.log("Global Sales", globalSalesForPlatform)
+      // console.log("Global Sales", globalSalesForPlatform)
 
       let initialValue = 0;
       let sumWithInitial = globalSalesForPlatform.reduce(
         (previousValue, currentValue) => previousValue + currentValue, initialValue).toFixed(2);// sumWithInitial returns a 16 digit decimal
                                                                                                 // .toFixed(2) cuts number off at 2 decimals
-      console.log("Sum", sumWithInitial);
+      // console.log("Sum", sumWithInitial);
       
-      return [platform, sumWithInitial, "silver"]
+      return [platform, parseFloat(sumWithInitial) , "blue"]
     });
 
-    console.log("Platform Arrays", platformArrays)
+    // console.log("Platform Arrays", platformArrays)
     
     
     const data = [
-        ["Platform", "Sales", { role: "style" }],
+        ["Platform", "Sales (in millions)", { role: "style" }],
         ...platformArrays
       ];
     
-      console.log("Data", data)
+      // console.log("Data", data)
       
       return data;
     }
